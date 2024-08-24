2fa467b92e9172c70469a932caf07f74c938a399
getor20 <ggetor20@gmail.com>
2024-08-25 00:04:18 +0200

    Update Background.cs

--- Assets/Scripts/Background_Scripts/Background.cs
+++ Assets/Scripts/Background_Scripts/Background.cs
@@ -1,19 +1,6 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    /*private void FixedUpdate()
-    {
-       transform.position = Camera.main.transform.position; 
-    }*/
-
-    /*public Transform target;
-
-    private void Update()
-    {
-       Vector2 position = target.position - transform.position;
-       
-        
-    }*/
+    
 }
