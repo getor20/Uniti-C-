9a6eeba5cfa2c67868d9f05eaa2cf8b1c9bb6c97
getor20 <ggetor20@gmail.com>
2024-08-31 11:46:19 +0200

    Update Player_Controller.cs

--- Assets/Scripts/Player_Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Scripts/Player_Controller.cs
@@ -49,7 +49,7 @@ public class Player_Controller : MonoBehaviour
 
     public void Acceleration(InputAction.CallbackContext context)
     {
-        speed = 10;
+        speed += 10;
     }
 
     public void OnJump(InputAction.CallbackContext context)
