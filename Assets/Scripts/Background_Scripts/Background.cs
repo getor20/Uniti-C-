e1c868b010a8e4c79034a0a4bcc72e49516a4d19
getor20 <ggetor20@gmail.com>
2024-09-25 01:10:01 +0200

    1

--- Assets/Scripts/Background_Scripts/Background.cs
+++ Assets/Scripts/Background_Scripts/Background.cs
@@ -2,9 +2,5 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    public Transform target;
-    private void Update()
-    {
-        transform.position = target.position;
-    }
+    
 }
