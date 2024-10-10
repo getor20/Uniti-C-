cde45d7192f974ce415f4960209f55d5e6a31040
getor20 <ggetor20@gmail.com>
2024-10-10 19:41:44 +0200

    1

--- Assets/Scripts/UI/Inventory/InventoryManager.cs
+++ Assets/Scripts/UI/Inventory/InventoryManager.cs
@@ -1,7 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InventoryManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject inventoryPanel;
+
+    private bool visibleInventoryPanel;
+
+    private void Start()
+    {
+        inventoryPanel.SetActive(visibleInventoryPanel);
+    }
+
+    public void OnToggle(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            visibleInventoryPanel = !visibleInventoryPanel;
+            inventoryPanel.SetActive(visibleInventoryPanel);
+        }
+    }
 }
